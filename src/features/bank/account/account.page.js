2e33416224f7ba09d146a0111d56e2934b537899
dantin-durand <durand.dantin.pro@gmail.com>
2022-03-30 02:50:58 +0200

    [fix] debug ws

--- src/features/bank/account/account.page.js
+++ src/features/bank/account/account.page.js
@@ -23,7 +23,7 @@ export default function BankAccount() {
       broadcaster: "pusher",
       wsHost: process.env.REACT_APP_WS_HOST,
       wsPort: process.env.REACT_APP_WS_PORT,
-      forceTLS: process.env.REACT_APP_WS_TLS,
+      forceTLS: Boolean(process.env.REACT_APP_WS_TLS),
       disableStats: true,
     });
 
